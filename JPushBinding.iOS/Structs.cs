a2d09cbf3275f12ac3344ba11e65f34915234c80
baichonglong <a364711322@sina.com>
2018-02-06 13:57:56 +0800

    JPush binding for ios.

---
+++ JPushBinding.iOS/Structs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace JPushBinding.iOS
+{
+    [Native]
+    public enum JPAuthorizationOptions : uint
+    {
+        None = 0,
+        Badge = (1 << 0),
+        Sound = (1 << 1),
+        Alert = (1 << 2)
+    }
+
+}
+
